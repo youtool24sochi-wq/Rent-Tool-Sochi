44342a5a16cea6019dce3108c8b444b2006ca9eb
Baimanchick <baikaimanchick@gmail.com>
2025-09-03 19:58:43 +0600

    [GIT] git work

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -9,10 +9,10 @@ import { useAppDispatch, useAppSelector } from '@/shared/hooks/reduxHook'
 import { refreshOAuth2Token, userMe } from '@/store/features/auth/authSlice'
 
 export default function AuthCallBack() {
+  const dispatch = useAppDispatch()
   const router = useRouter()
   const api = useNotificationApi()
   const searchParams = useSearchParams()
-  const dispatch = useAppDispatch()
   const isAuth = useAppSelector((state) => state.auth.isAuth)
   const [checked, setChecked] = React.useState(false)
 
