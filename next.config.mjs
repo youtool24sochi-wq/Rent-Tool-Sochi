01b669afed836f43a3d9dc4e0913aa1b18019a4f
ArturInspector <bikk_artur@mail.ru>
2025-09-08 13:24:43 +0600

    nexteslintignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'api.renttoolspeed.ru' },
